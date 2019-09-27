d4dd1c5756fce23c3c7fc23df89180690377fd83
Elizabeth Blackburn <e.r.blackburn@gmail.com>
2019-09-27 16:26:08 -0400

    restore readme, add boilerplate readme

--- lib/phoebe.ex
+++ lib/phoebe.ex
@@ -6,4 +6,10 @@ defmodule phoebe do
   Contexts are also responsible for managing your data, regardless
   if it comes from the database, an external API or others.
   """
+
+  use Application
+
+  def start(_type, _args) do
+    
+  end
 end
